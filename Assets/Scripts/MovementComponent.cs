ed5d1d75de3753149bb4cdfbcce03f45d99e02e0
Grim <TheGrimsey@users.noreply.github.com>
2020-05-15 10:15:03 +0200

    Add StatsComponent & MovementComponent.

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(StatsComponent))]
+public class MovementComponent : MonoBehaviour
+{
+    StatsComponent _statsComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _statsComponent = GetComponent<StatsComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StatsComponent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsComponent : MonoBehaviour
+{
+    //Current Health
+    [SerializeField]
+    int _health;
+    public int Health => _health;
+    
+    //Max Health.
+    [SerializeField]
+    int _maxHealth = 100;
+    public int MaxHealth => _maxHealth;
+
+    //Amount of Damage to deal when attacking.
+    [SerializeField]
+    int _attackPower = 8;
+    public int AttackPower => _attackPower;
+
+    //How far we can reach when attacking (16 = 1 tile)
+    [SerializeField]
+    int _attackReach = 16;
+    public int AttackReach => _attackReach;
+
+    //How fast we move (16 = 1 tile)
+    [SerializeField]
+    int _movementSpeed = 16;
+    public int MovementSpeed => _movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetHealth(); 
+    }
+
+    public void ResetHealth()
+    {
+        _health = _maxHealth;
+    }
+
+    public void Damage(int damage)
+    {
+        _health = Mathf.Clamp(Health - damage, 0, MaxHealth); 
+    }
+}
